fd70a718629f032fd5645c517935aa6af4e3f963
David Asaolu <67129211+dha-stix@users.noreply.github.com>
2024-10-14 10:56:35 +0100

    Update page.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/page.tsx
@@ -1,14 +1,11 @@
 import WorkspaceLinksClient from "./page-client";
 
-// export default function WorkspaceLinks() {
-//   return <WorkspaceLinksClient />;
-// }
 export default function WorkspaceLinks() {
   return (
-    <div className="px-2 w-[70%] mx-auto text-center my-3">
-      <h2 className="text-2xl mb-3 text-orange-500">Testing how CodeRabbit works</h2>
+   
+      <h2 className="text-2xl mb-3 text-orange-500">Testing how CodeRabbit works
       <p>CodeRabbit is an AI-powered code reviewer for your code repositories. It provides quick, context-aware code review feedback and line-by-line suggestions, significantly reducing manual review time.</p>
-    </div>
+
 
   )
 }
